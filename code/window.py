a7a4fd9954bfae8af0bd37b46123552fea9959d7
Sam Kennerly <samkennerly@gmail.com>
2020-03-26 23:45:12 -0400

    Add ContagionWindow class and scratch notebook

--- code/window.py
+++ code/window.py
@@ -1,5 +1,5 @@
 from collections.abc import Sequence
-
+from itertools import accumulate
 
 def convolved(values, window):
     """ int or float: Discrete convolution of two sequences. """
@@ -11,17 +11,20 @@ class ContagionWindow(Sequence):
     UNDER CONSTRUCTION
     """
 
-    def __init__(self, values=(1,), **kwargs):
-        probs, pop = dict(), kwargs.pop
-        probs['case'] = float(pop('case', 1))
-        probs['fatal'] = float(pop('p_fatal', 0))
-        probs['transmit'] = float(pop('p_transmit', 0.5))
-        probs['vaccinate'] = float(pop('p_vaccinate', 0))
+    def __init__(self, values, **kwargs):
+        p, pop = dict(), kwargs.pop
+        p['case'] = float(pop('case', 1))
+        p['fatal'] = float(pop('fatal', 0))
+        p['transmit'] = float(pop('transmit', 0.5))
+        p['vaccinated'] = float(pop('vaccinated', 0))
         if kwargs:
             raise ValueError(f"unknown keyword arguments: {sorted(kwargs)}")
 
-        self.probs = probs
-        self.values = tuple(map(float, values))
+        self.p = p
+        self.values = list(map(float, values))
+
+    def __call__(self, counts, **kwargs):
+        raise NotImplementedError
 
     def __getitem__(self, i):
         return self.values[i]
@@ -33,7 +36,8 @@ class ContagionWindow(Sequence):
         return len(self.values)
 
     def __str__(self):
-        return "\n".join(str(self.values), *[ f"{k}: {v}" for k, v in self.probs ])
+        name = type(self).__name__
+        vstr = f"values: {self.values}"
+        pstr = "\n".join( f"{k}: {v}" for k, v in self.p.items() )
 
-
-
+        return f"{name}\n{vstr}\n{pstr}"
