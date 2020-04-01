5aab6e08b8729c98ad07be32473371d1cd7f1cef
Sam Kennerly <samkennerly@gmail.com>
2020-03-31 20:38:38 -0400

    Add etl.kaggle

--- code/etl.py
+++ code/etl.py
@@ -7,13 +7,24 @@ from pandas import read_csv
 
 DATADIR = Path(__file__).resolve().parent.parent / "data"
 
-def kaggle():
-    raise NotImplementedError
+def kaggle(path="kaggle/global/input/train.csv", **kwargs):
+    """ DataFrame: Daily total cases from Kaggle Global Forecasting contest. """
+    kwargs.setdefault('parse_dates', ['Date'])
+    kwargs.setdefault('index_col', 'Country_Region Province_State Date'.split())
+
+    return (
+        read_csv(DATADIR / path, **kwargs)
+        .rename(columns={'ConfirmedCases': 'confirmed', 'Fatalities': 'deceased'})
+        .drop(columns='Id').astype(int)
+    )
 
 def rivm(path="rivm/report.csv", **kwargs):
     """ DataFrame: Daily new cases from RIVM (Netherlands) PDF report. """
-    path = DATADIR / path
-    kwargs.setdefault('parse_dates', True)
+    kwargs.setdefault('parse_dates', ['date'])
     kwargs.setdefault('index_col', 'date')
 
-    return read_csv(path, **kwargs).sort_index(axis=1).resample('D').sum()
+    return (
+        read_csv(DATADIR / path, **kwargs)
+        .rename_axis(None).sort_index(axis=1)
+        .resample('D').sum().astype(int)
+    )
