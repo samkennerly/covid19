c8d00ebf41d6ffc1b31f4f32443719e45a5d2285
Sam Kennerly <samkennerly@gmail.com>
2020-04-01 00:29:06 -0400

    Add scratch book

--- code/contagion.py
+++ code/contagion.py
@@ -1,8 +1,8 @@
-from collections import namedtuple
-from itertools import accumulate
+from collections import deque, namedtuple
 
 STATES = (
     "deceased",
+    "exposed",
     "infectious",
     "quarantined",
     "recovered",
@@ -10,27 +10,28 @@ STATES = (
     "vaccinated",
 )
 
-def convolved(values, window):
+
+def convolution(values, window):
     """ int or float: Convolution of two sequences. """
     return sum(w * x for w, x in zip(window, reversed(values)))
+
 
 class Contagion:
     """
     UNDER CONSTRUCTION
     """
-    States = namedtuple("States", STATES)
+    Counts = namedtuple("Counts", STATES)
 
-    def __init__(self, window, **kwargs):
+    def __init__(self, window=None, **kwargs):
         window = tuple(map(float, window))
         params = {
+            "beta": float(kwargs.pop("beta", 1.0)),
             "dprob": float(kwargs.pop("dprob", 0.1)),
-            "iprob": float(kwargs.pop("iprob", 1.0)),
-            "qprob": float(kwargs.pop("qprob", 0.5)),
-            "vprob": float(kwargs.pop("vprob", 0.0)),
             "dtime": int(kwargs.pop("dtime", len(window))),
-            "itime": int(kwargs.pop("itime", 1)),
+            "qprob": float(kwargs.pop("qprob", 0.5)),
             "qtime": int(kwargs.pop("qtime", len(window))),
-            "vtime": int(kwargs.pop("vtime", 0)),
+            "total": int(kwargs.pop("total", 100)),
+            "vprob": float(kwargs.pop("vprob", 0.0)),
         }
         if kwargs:
             raise ValueError(f"unknown keyword arguments: {sorted(kwargs)}")
@@ -38,21 +39,22 @@ class Contagion:
         self.params = params
         self.window = window
 
-    def __call__(self, exposures, **kwargs):
+    def __call__(self, deltas, **kwargs):
         params = {**self.params, **kwargs}
         window = self.window
 
         wtime = len(window)
 
-        exposed = (1 - vprob - sum(deltas) / n) * convolved(deltas, window)
+
+        exposed = s * sum(w * x for w, x in zip(reversed(window), values))
 
         delayed = deltas[-wtime] if (len(deltas) > tau) else 0
 
         deceased = int(mu * delayed)
         recovered = delayed - deceased
 
-        infectious = convolved(deltas, *(w > 0 for w in window))
-        quarantined = convolved(deltas, *(w == 0 for w in window))
+        infectious = convolution(deltas, *(w > 0 for w in window))
+        quarantined = convolution(deltas, *(w == 0 for w in window))
 
         vaccinated = int(vax_rate * unexposed)
         susceptible = unexposed - vaccinated
